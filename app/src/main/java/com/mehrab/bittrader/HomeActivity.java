e7003e255ccb56f0cb6f9e4f3f2db7cc321136c6
mehrabha <mehrabhfz@gmail.com>
2019-08-26 13:58:23 -0400

    graphview crashing. Asked questions on stackoverflow

--- app/src/main/java/com/mehrab/bittrader/HomeActivity.java
+++ app/src/main/java/com/mehrab/bittrader/HomeActivity.java
@@ -3,9 +3,39 @@ package com.mehrab.bittrader;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeActivity extends AppCompatActivity {
+    private List<DataPoint> datapoints_ = new ArrayList<DataPoint>();
+    private GraphView graph = (GraphView) findViewById(R.id.main_graph);
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //generateDatapoints();
+    }
+
+    // Generates datapoints for datapoints_
+    private void generateDatapoints() {
+        for(int i = 0; i < 10; i++) {
+            //datapoints_.add(new DataPoint(i, i));
+        }
+        updateGraph();
+    }
+
+    // Updates graph using the entries in datapoints_
+    private void updateGraph() {
+        //DataPoint[] datapoints = new DataPoint[datapoints_.size()];
+
+        //for (int i = 0; i < datapoints_.size(); i++) {
+          //  datapoints[i] = datapoints_.get(i);
+        //}
+
+        //graph.addSeries(new LineGraphSeries(datapoints));
     }
 }
