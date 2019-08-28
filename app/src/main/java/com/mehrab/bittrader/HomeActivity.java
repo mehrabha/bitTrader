6d0d80162e3a8e53792fefaf2f40720fc9763b7e
mehrabha <mehrabhfz@gmail.com>
2019-08-28 14:43:07 -0400

    fetch current btc price from coindesk api

--- app/src/main/java/com/mehrab/bittrader/HomeActivity.java
+++ app/src/main/java/com/mehrab/bittrader/HomeActivity.java
@@ -3,24 +3,87 @@ package com.mehrab.bittrader;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class HomeActivity extends AppCompatActivity {
+    public static final String TAG = "HomeActivity.java";
+    private RequestQueue queue;
+    private static final String CURRENT_PRICE_URL =
+            "https://api.coindesk.com/v1/bpi/currentprice.json";
+    private static final String HISTORICAL_PRICE_URL =
+            "https://api.coindesk.com/v1/bpi/historical/close.json";
+
+
+
     private List<DataPoint> datapoints_ = new ArrayList<DataPoint>();
+    private String currentPrice_ = "";
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        updatePrice();
         generateDatapoints();
     }
 
+    // fetches current market price
+    private void updatePrice() {
+        // Make API call
+        queue = Volley.newRequestQueue(this);
+        JsonObjectRequest request = new JsonObjectRequest(
+            Request.Method.GET,
+            CURRENT_PRICE_URL,
+            null,
+            new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    // Extract data on response and update price
+                    try {
+                        JSONObject Bpi = response.getJSONObject("bpi");
+                        try {
+                            JSONObject Usd = Bpi.getJSONObject("USD");
+                            currentPrice_ = "$" + Usd.getString("rate");
+
+                            // Update price
+                            TextView btc_price = (TextView) findViewById(R.id.btc_price);
+                            btc_price.setText(currentPrice_);
+                        } catch (JSONException e){
+                            e.printStackTrace();
+                            Log.e(TAG, "rate not found");
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        Log.e(TAG, "BPI not found");
+                    }
+                }
+            },
+            new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e(TAG, "onErrorResponse: Cannot fetch data from api");
+                }
+            }
+        );
+        queue.add(request);
+    }
     // Generates datapoints for datapoints_
     private void generateDatapoints() {
         for(int i = 0; i < 10; i++) {
